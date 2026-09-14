d36fd2299b46846d8413c4616705d66f11d00b3a
rafeger <rafeger@student.42.fr>
2026-09-14 12:00:56 +0200

    yes

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -10,22 +10,23 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-    public:
-	//on vole les 
-        typedef typename std::deque<T>::iterator       iterator;
-        typedef typename std::deque<T>::const_iterator const_iterator;
+	public:
+		//on vole les 
+	typedef typename std::deque<T>::iterator       iterator;
+	typedef typename std::deque<T>::const_iterator const_iterator;
 
-        MutantStack() {}
-        MutantStack(const MutantStack& other) : std::stack<T>(other) {}
-        MutantStack& operator=(const MutantStack& other)
-        {
-            std::stack<T>::operator=(other);
-            return *this;
-        }
-        ~MutantStack() {}
+	MutantStack();
+	MutantStack(const MutantStack& other) : std::stack<T>(other);
+	MutantStack &operator=(const MutantStack& other);
+	~MutantStack();
+	{
+		std::stack<T>::operator=(other);
+		return *this;
+	}
 
-        iterator begin() { return this->c.begin(); }
-        iterator end()   { return this->c.end(); }
-        const_iterator begin() const { return this->c.begin(); }
-        const_iterator end()   const { return this->c.end(); }
+
+	iterator begin() { return this->c.begin(); }
+	iterator end()   { return this->c.end(); }
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end()   const { return this->c.end(); }
 };
